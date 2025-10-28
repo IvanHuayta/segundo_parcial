6f0a8ce872cca178b39e47d3a488e99821521b82
IvanHuayta <107371442+IvanHuayta@users.noreply.github.com>
2025-10-27 21:57:40 -0400

    crear modelo empleado

---
+++ app/Models/Empleado.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    //
+        protected $table='empleados';
+
+    protected $fillable=[
+        'nombre',
+        'apellido',
+        'correo',
+        'salario'
+    ];
+
+
+
+}
